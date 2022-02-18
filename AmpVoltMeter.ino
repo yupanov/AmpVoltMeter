986a243580996747bf5092bbb487d8c1b4bb2a65
Yurik280 <Yuiyuribaires@gmail.com>
2022-02-18 20:01:46 -0300

    Voltmeter and i2c lcd added

--- AmpVoltMeter.ino
+++ AmpVoltMeter.ino
@@ -1,18 +1,51 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
 const uint8_t mAmpStep = 66;
+const double divResOne = 995.0; // kOhm
+const double divResTwo = 219.0; // kOhm
+const double divRatio =  divResTwo / (divResOne + divResTwo); 
+const double vRef = 3.94;
 
 // pins:
-const uint8_t currentPin = 14;
+const uint8_t currentPin = A0;
+const uint8_t voltagePin = A1;
 
 float current = 0.0;
+float voltage = 0.0;
 
 void setup() {
-pinMode(currentPin, INPUT);
-Serial.begin(9600);
+  pinMode(currentPin, INPUT);  
+  pinMode(voltagePin, INPUT);
+  Serial.begin(9600);
+
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-current = (analogRead(currentPin) - 512) / 1000 * mAmpStep;
-Serial.print("Current = ");
-Serial.print(current, 2);
-Serial.println(" A");
+
+  current = (analogRead(currentPin) - 510) / 1000.0 * mAmpStep; // Amps
+  voltage = analogRead(voltagePin) / divRatio * vRef / 1024.0; // Volts
+  Serial.print("Current = ");
+  Serial.print(current, 2);
+  Serial.println(" A   ");
+
+  //lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Current = ");
+  lcd.print(current, 2);
+  lcd.print(" A   ");
+
+  Serial.print("Voltage = ");
+  Serial.print(voltage, 2);
+  Serial.println(" V   ");
+
+  //lcd.clear();
+  lcd.setCursor(2, 1);
+  lcd.print("Voltage = ");
+  lcd.print(voltage, 2);
+  lcd.print(" V   ");
+  delay(500);
 }
